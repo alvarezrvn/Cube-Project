e62824889aa9138edfdea3191b621bcfb83156cc
General-PC <araven@nmsu.edu>
2019-03-03 19:29:37 -0700

    Basic assets for rubik's cube

---
+++ Cube Project/Assets/RotateCube.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public Transform Cube;
+    public Vector3 turn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a") && Input.GetKeyUp("a"))
+        {
+            Debug.Log("here");
+            
+            Cube.transform.Rotate(0, 90 * Time.deltaTime, 0);
+        }
+        //Cube.transform.Rotate(0, 90 * Time.deltaTime, 0);
+    }
+}
